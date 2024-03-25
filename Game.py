4bb285e83449204dfe2b93169e7f34bf1e7f5827
Evita <117149941+221RDB187@users.noreply.github.com>
2024-03-25 12:50:19 +0200

    Update Game.py

--- Game.py
+++ Game.py
@@ -1,24 +1,7 @@
 # Game.py
 
 import random
-
-def can_reduce_to_10_or_less(n):
-    if n <= 10:
-        return True
-    if n % 2 == 0 and can_reduce_to_10_or_less(n // 2):
-        return True
-    if n % 3 == 0 and can_reduce_to_10_or_less(n // 3):
-        return True
-    if n % 4 == 0 and can_reduce_to_10_or_less(n // 4):
-        return True
-    return False
-
-numbers_that_can_reduce = [n for n in range(20000, 30001) if n % 12 == 0 and can_reduce_to_10_or_less(n)]
-
-def randomNumbers():
-    if not numbers_that_can_reduce:  # Check if the list is empty or None
-        return []  # Return an empty list if there's nothing to sample from
-    return random.sample(numbers_that_can_reduce, 5)
+from graphics import endGameScreen
 
 
 def scoreUpdate(divisor):
